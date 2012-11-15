101a416742dd598a28b0781b7da4a9cebe1f36be
Brad Hong <breadval@hotmail.com>
2012-11-15 11:45:24 +0900

    Update BaasioStartupProject/src/com/baasio/sample/startup/BaasioConfig.java
    url changed

--- BaasioStartupProject/src/com/baasio/sample/startup/BaasioConfig.java
+++ BaasioStartupProject/src/com/baasio/sample/startup/BaasioConfig.java
@@ -1,8 +1,7 @@
-
 package com.baasio.sample.startup;
 
 public class BaasioConfig {
-    public static final String BAASIO_URL = "https://stgapi.baas.io";
+    public static final String BAASIO_URL = "https://api.baas.io";
 
     public static final String ORGANIZATION_ID = PUT_YOUR_MEMBER_ID; // member ID(organization id)
 
